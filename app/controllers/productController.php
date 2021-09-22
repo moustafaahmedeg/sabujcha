08b4f62bc6b140f097f57e8f0aaedf7896f36cd6
Moustafa Ahmed <moustafaahmedprogrammer@gmail.com>
2021-09-22 18:50:46 +0200

    Structure MVC

---
+++ app/controllers/productController.php
@@ -0,0 +1,12 @@
+<?php
+
+    require_once '../components/model.php';
+    require_once '../components/controller.php';
+    require_once '../components/view.php';
+
+    class ProductController extends controller {
+
+        public function getProduct() {
+
+        }
+    }
